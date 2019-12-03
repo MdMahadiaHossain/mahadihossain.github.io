066b3a269695e581cee425858f45a88e6fc44333
MdMahadiaHossain <mahadihossainpvt@gmail.com>
2019-12-03 22:36:59 +0600

    Update webRtc.js

--- webRtc.js
+++ webRtc.js
@@ -1,24 +1,22 @@
-
 let v = document.getElementById("localVideo");
 let localStreem;
 
-let promise =  navigator.mediaDevices.getUserMedia({audio:true,video:true});
+let promise = navigator.mediaDevices.getUserMedia({ audio: true, video: true });
 
-promise.then(mediaStream =>{
-    v.srcObject=mediaStream;
-    localStreem=mediaStream; 
+promise.then(mediaStream => {
+  v.srcObject = mediaStream;
+  localStreem = mediaStream;
+  // local stream is being added in RTCPeerConnection to send to peer
+  localStreem.getTracks.array.forEach(track => {
+    pc.addTrack(track, localStreem);
+  });
 });
 
-let pc = new RTCPeerConnection(null)
+let pc = new RTCPeerConnection(null);
 
- pc.addEventListener("icecandidate",(event)=>{
-    // this candidate will be used in other peer.
-    console.log(event.candidate);
-
-});
-// local stream is being added in RTCPeerConnection to send to peer
-localStreem.getTracks.array.forEach(track => {
-    pc.addTrack(track,localStreem);
+pc.addEventListener("icecandidate", event => {
+  // this candidate will be used in other peer.
+  console.log(event.candidate);
 });
 
 // create offer
